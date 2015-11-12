b1c9cfce09e44cf6729443a97e065235c8b13c13
wes.ehrlichman <wes.ehrlichman@asynchrony.com>
2015-11-12 10:00:48 -0600

    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float movementSpeed = 120f;
+	public float rotationSpeed = 120f;
+
+	private Rigidbody rigidBody;
+
+	void Start() {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		var horizontal = -Input.GetAxisRaw ("Horizontal") * Time.deltaTime;
+		var vertical = -Input.GetAxisRaw ("Vertical") * Time.deltaTime;
+		transform.Translate (new Vector3 (horizontal * movementSpeed, 0f, vertical * movementSpeed));
+//		rigidBody.AddForce (new Vector3 (0f, -vertical * movementSpeed, 0f));
+		
+		var horizontal2 = Input.GetAxisRaw ("Horizontal2") * Time.deltaTime;
+		var vertical2 = Input.GetAxisRaw ("Vertical2") * Time.deltaTime;
+		transform.Rotate (new Vector3 (vertical2 * rotationSpeed, horizontal2 * rotationSpeed, 0f));
+	}
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	public Vector3 rotation;
+
+	// Use this for initialization
+	void Start () {
+		rotation = new Vector3 (1f, 2f, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(rotation, Space.World);
+	}
+}
